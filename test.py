791626d32a74294fdb8f78ef7e9534d7b5d3aaaa
Brett Feltmate <the.bretter.email@gmail.com>
2023-11-06 14:37:52 -0400

    Removed redundant tester class.

--- test.py
+++ test.py
@@ -2,25 +2,24 @@ import sys
 import os
 import time
 
-from test_output import testOutput
-
+from OptiTracker import OptiTracker
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-tester = testOutput()
+OptiTracker = OptiTracker()
 
-tester.init_client()
+OptiTracker.init_client()
 
-tester.start_client()
+OptiTracker.start_client()
 
 wait_until = time.time() + 0.1
 
 while time.time() < wait_until:
     pass
 
-tester.save_description()
+OptiTracker.save_description()
 
-tester.stop_client()
+OptiTracker.stop_client()
 
 exit()

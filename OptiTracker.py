5702ae1906838ebeaec2933c434c90864067d481
Brett Feltmate <the.bretter.email@gmail.com>
2023-11-06 14:36:15 -0400

    Forgot there was already a test class.

--- OptiTracker.py
+++ OptiTracker.py
@@ -1,80 +1,52 @@
+import sys
+import os
+import csv
+from functools import reduce
 from Resources.API.Official.PythonClient import NatNetClient
 
+# TODO: is this necessary?
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 class OptiTracker:
     def __init__(self) -> None:
         self.client = self.init_client()
-
+        self.desc_dict = None
 
     # Create NatNetClient instance
-    def init_client(self,
-            server="127.0.0.1", multicast="239.255.42.99",
-            commandPort=1510, dataPort=1511,
-            motiveVersion=(3,0,1,0), verbose=False,
-            uses_skeleton_listener=False,
-            uses_rigid_body_listener=False,
-            uses_marker_set_listener=False,
-            uses_data_description_listener=True,
-            uses_frame_listener=True) -> object:
+    def init_client(self) -> object:
         
         # Spawn client
-        client = NatNetClient(
-            # server=server, multicast=multicast,
-            # commandPort=commandPort, dataPort=dataPort,
-            # motiveVersion=motiveVersion, verbose=verbose
-        )
-
-        # Activate requested listeners by assinging them to client
-        # Active listener functions get called on every frame
-        if uses_skeleton_listener:
-            client.skeletonListener = self.skeleton_listener
-        
-        if uses_rigid_body_listener:
-            client.rigidBodyListener = self.rigid_body_listener
-
-        if uses_marker_set_listener:
-            client.markerSetListener = self.marker_set_listener
-
-        if uses_frame_listener:
-            client.new_frame_listener = self.frame_listener
-
-        if uses_data_description_listener:
-            client.data_description_listener = self.data_description_listener
-
-        # Activate client
-        # TODO: How best to inform user if client setup fails?
-        client.run()
+        client = NatNetClient()
+        client.data_description_listener = self.get_description
 
         return client
     
-    # Returns summary description of all active assets
-    # Called once when initing.
-    def get_asset_descriptions(self) -> dict:
-        pass
-    
-    # When active, records frame-data of all active assets
-    # Activated by default, called once per frame-rate
-    def frame_listener(self) -> None:
-        pass
+    def start_client(self):
+        self.client.run()
 
-    def data_description_listener(self, data_descs):
-        pass
+    def stop_client(self):
+        self.client.shutdown()
 
-    # When active, records frame-data of all active skeleton assets
-    # Deactivated by default, called once per frame-rate
-    def skeleton_listener(self) -> None:
-        pass
+    def get_description(self, desc_dict):
+        self.desc_dict = desc_dict
 
-    # When active, records frame-data of all active rigid body assets
-    # Deactivated by default, called once per frame-rate
-    def rigid_body_listener(self) -> None:
-        pass
+    def save_description(self):
+        if self.desc_dict is None:
+            print("crap")
+        else:
+            keys = reduce(set.union, map(set, self.desc_dict.values()))
+            print('keys')
+            with open('out\desc_dict.csv', 'w') as file:
+                print("file opened")
+                w = csv.writer(file, delimiter = '\t')
+                w.writerow([''] + self.desc_dict.keys())
+                for key in keys:
+                    w.writerow(
+                        [key] + [subdict.get(key, '') for subdict in self.desc_dict.values()]
+                    )
 
-    # When active, records frame-data of all active markerset assets 
-    # Deactivated by default, called once per frame-rate
-    def marker_set_listener(self) -> None:
-        pass
-    
+
 
 
 
